839c5ccacf16f6fd08f6d8d9524326fcc6dbfeb2
Bo01la <abdoehab30@gmail.com>
2025-08-15 16:44:56 +0300

    finished the features section and modified the scroll behavior of nav

--- client/src/sections/Features.jsx
+++ client/src/sections/Features.jsx
@@ -1,11 +1,11 @@
 import { Element } from "react-scroll";
-import { features } from "../constents";
+import { details, features } from "../constents";
 import Button from "../components/Button";
 
 const Features = () => {
   return (
     <section>
-      <Element name="features">
+      <Element name="Features">
         <div className="container">
           <div className="relative flex md:flex-wrap flex-nowrap border-2 border-s3 rounded-7xl md:overflow-hidden max-md:flex-col feature-after md:g7 max-md:border-none max-md:rounded-none max-md:gap-3 ">
             {features.map(({ id, icon, text, title, caption, button }) => (
@@ -31,6 +31,25 @@ const Features = () => {
                 <Button icon={button.icon}>{button.title}</Button>
               </div>
             ))}
+
+            <ul className="relative flex justify-around flex-grow px-[5%] border-2 border-s3 rounded-7xl max-md:hidden">
+              <div className="absolute bg-s3/20 top-[38%] left-0 right-0 w-full h-[1px] z-10 " />
+              {details.map(({ id, title, icon }) => (
+                <li key={id} className="relative pt-16 px-4 pb-14 ">
+                  <div className="absolute top-8 bottom-0 left-1/2 bg-s3/20 w-[1px] h-full  z-10" />
+                  <div className="flex items-center justify-center mx-auto mb-3 border-2 border-s2 rounded-full hover:border-s4 transition-all duration-500 shadow-500 size-20">
+                    <img
+                      src={icon}
+                      alt={title}
+                      className="size-17/20 object-contain z-20"
+                    />
+                  </div>
+                  <h3 className="relative z-2 max-w-36 mx-auto my-0 base-small text-center uppercase">
+                    {title}
+                  </h3>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </Element>
