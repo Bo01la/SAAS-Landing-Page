3071ff061b2ee52022b56284da1a8f70bbb1469e
Bo01la <abdoehab30@gmail.com>
2025-08-16 19:31:45 +0300

    replacer react-slidedown with react-collapse due to incompitability why deploying

--- client/src/components/FaqBox.jsx
+++ client/src/components/FaqBox.jsx
@@ -1,33 +1,33 @@
 import { useState } from "react";
 import clsx from "clsx";
-import { SlideDown } from "react-slidedown";
-import "react-slidedown/lib/slidedown.css";
+import { Collapse } from "react-collapse";
 
 const FaqBox = ({ index, item }) => {
   const [activeId, setActiveId] = useState(null);
   const active = activeId === item.id;
 
   return (
-    <div className="relative z-2 mb-16 ">
+    <div className="relative z-2 mb-16">
       <div
         className="group relative flex cursor-pointer items-center justify-between gap-10 px-7"
         onClick={() => {
-          setActiveId(activeId === item.id ? null : item.id);
+          setActiveId(active ? null : item.id);
         }}
       >
-        <div className="flex-1 ">
+        <div className="flex-1">
           <div className="small-compact mb-1.5 text-p3 max-lg:hidden">
             {index < 10 ? "0" : ""} {index}
           </div>
           <div
             className={clsx(
               "h6 text-p4 transition-colors duration-500 max-md:flex max-md:min-h-20 max-md:items-center",
-              active && "max-lg:text-p1 "
+              active && "max-lg:text-p1"
             )}
           >
             {item.question}
           </div>
         </div>
+
         <div
           className={clsx(
             "faq-icon relative flex size-12 items-center justify-center rounded-full border-2 border-s2 shadow-400 transition-all duration-500 group-hover:border-s4",
@@ -40,16 +40,18 @@ const FaqBox = ({ index, item }) => {
         </div>
       </div>
 
-      <SlideDown>
-        {active && <div className="body-3 px-7 py-3.5">{item.answer}</div>}
-      </SlideDown>
+      {/* react-collapse replaces SlideDown */}
+      <Collapse isOpened={active}>
+        <div className="body-3 px-7 py-3.5">{item.answer}</div>
+      </Collapse>
+
       <div
         className={clsx(
           "absolute g5 -bottom-7 -top-7 left-0 right-0 -z-1 rounded-3xl opacity-0 transition-opacity duration-500",
           active && "opacity-100"
         )}
       >
-        <div className="g4 absolute inset-0.5 -z-1 rounded-3xl  " />
+        <div className="g4 absolute inset-0.5 -z-1 rounded-3xl" />
         <div className="absolute left-8 top-0 h-0.5 w-40 bg-p1" />
       </div>
     </div>
